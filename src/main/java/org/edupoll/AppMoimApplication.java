86526f5093e86c5354c78ba3e112c10a5c9187a5
coco <coco@gmail.com>
2023-07-04 15:05:22 +0900

    feat:수정

--- src/main/java/org/edupoll/AppMoimApplication.java
+++ src/main/java/org/edupoll/AppMoimApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppMoimApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {   
+		
+		
 		SpringApplication.run(AppMoimApplication.class, args);
 	}
 
